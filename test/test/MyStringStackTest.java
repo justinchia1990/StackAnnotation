6a4c4883b401ca7de7ea5e91d6af85b6dc3dd278
justinchia <justin.chia@ncs.com.sg>
2020-12-01 14:06:33 +0800

    fail the test case

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -34,6 +34,7 @@ public class MyStringStackTest extends TestCase {
 		String o1 = null;  
 		MyStringStack stack = new MyStringStack();
 		stack.push (s1);
+		stack.push (s1);
         assertFalse (stack.isEmpty());
 	}
 
